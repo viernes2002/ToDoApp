bece2d2060e88250e3ed6f49146c8cb4feec011b
viernes2002 <jomihergom@gmail.com>
2024-03-04 13:14:12 -0500

    1234

--- todo/model/todo.py
+++ todo/model/todo.py
@@ -1,8 +1,17 @@
 class Todo:
-    pass
+    def __init__(self, code_id: int, title: str, description: str):
+        self.code_id = code_id
+        self.title = title
+        self.description = description
+        self.completed = False
+        self.tags = []
 
+    def mark_completed(self):
+        self.completed = True
 
-class TodoBook:
-    pass
+    def add_tags(self, tag:str):
+        if tag not in self.tags:
+            self.tags.append(tag)
 
-
+    def __str__(self):
+        return f"{self.code_id}-{self.title}"
